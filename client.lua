400e26c1d0a0a22517ce94e5c33677d8f9e6b58c
Swqpping <58861408+Swqppingg@users.noreply.github.com>
2021-03-04 21:54:08 +0100

    optimized tazer effect

--- client.lua
+++ client.lua
@@ -255,38 +255,39 @@ if Config.crouch then
     -----------------------------------------------------------------------------------
     -- Tazer effect
     if Config.tazereffect then
-    local isTaz = false
-    Citizen.CreateThread(function()
-        while true do
-            Citizen.Wait(0)
-            
-            if IsPedBeingStunned(GetPlayerPed(-1)) then
-                
-                SetPedToRagdoll(GetPlayerPed(-1), 5000, 5000, 0, 0, 0, 0)
-                
-            end
-            
-            if IsPedBeingStunned(GetPlayerPed(-1)) and not isTaz then
-                
-                isTaz = true
-                SetTimecycleModifier("REDMIST_blend")
-                ShakeGameplayCam("FAMILY5_DRUG_TRIP_SHAKE", 1.0)
-                
-            elseif not IsPedBeingStunned(GetPlayerPed(-1)) and isTaz then
-                isTaz = false
-                Wait(5000)
-                
-                SetTimecycleModifier("hud_def_desat_Trevor")
-                
-                Wait(10000)
-                
-          SetTimecycleModifier("")
-                SetTransitionTimecycleModifier("")
-                StopGameplayCamShaking()
-            end
-        end
-    end)
-    end
+local isTaz = false
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		ped = PlayerPedId()
+		
+		if IsPedBeingStunned(ped) then
+			
+			SetPedToRagdoll(ped, 5000, 5000, 0, 0, 0, 0)
+			
+		end
+		
+		if IsPedBeingStunned(ped) and not isTaz then
+			
+			isTaz = true
+			SetTimecycleModifier("REDMIST_blend")
+			ShakeGameplayCam("FAMILY5_DRUG_TRIP_SHAKE", 1.0)
+			
+		elseif not IsPedBeingStunned(ped) and isTaz then
+			isTaz = false
+			Wait(5000)
+			
+			SetTimecycleModifier("hud_def_desat_Trevor")
+			
+			Wait(10000)
+			
+      		SetTimecycleModifier("")
+			SetTransitionTimecycleModifier("")
+			StopGameplayCamShaking()
+		end
+	end
+end)
+end
     -------------------------------------------------------------------------
     -- No reticle
     if Config.noreticle then
