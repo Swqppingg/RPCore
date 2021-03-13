4ed7f4c090e0845a9f97a36e12be3306a3f9c107
Swqpping <swqpping@gmail.com>
2021-03-13 18:30:24 +0100

    fixed neverwanted

--- client.lua
+++ client.lua
@@ -647,17 +647,15 @@ end
 ------------------------------------------------------------------
 -- Never wanted script --
 if Config.neverwanted then
-    Citizen.CreateThread(function()
-        while true do
-            Citizen.Wait(10)
-            for i = 1, 12 do
-                EnableDispatchService(i, false)
-            end
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if GetPlayerWantedLevel(PlayerId()) ~= 0 then
             SetPlayerWantedLevel(PlayerId(), 0, false)
             SetPlayerWantedLevelNow(PlayerId(), false)
-            SetPlayerWantedLevelNoDrop(PlayerId(), 0, false)
         end
-    end)
+    end
+end)
 ------------------------------------------------------------------
 -- Remove parked vehicles --
 if Config.removeparkedvehicles then
